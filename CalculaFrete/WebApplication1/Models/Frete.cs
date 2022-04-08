1abea8d4dfeef90f9bce9b54f5cb9a4b77ee62a9
belsanpesou <=>
2022-04-08 01:20:26 -0300

    Mudando o nome da solucao

---
+++ CalculaFrete/WebApplication1/Models/Frete.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculaFreteApplication.Models
+{
+    public class Frete
+    {
+        public int cep { get; set; }
+        private ICorreioService correioService;
+        public double peso { get; set; }
+
+
+
+        public Frete(ICorreioService _correioService)
+        {
+            correioService = _correioService;
+        }
+        public double CalculaFrete() { return correioService.CalculaFrete(); }
+    }
+}
