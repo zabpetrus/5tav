50c55fac43a1c64f89f488a90dda450b825613f1
Izabel Souza <zabpetrus@gmail.com>
2022-05-15 00:29:52 -0300

    Corrigindo controller e adicionando viewModel

---
+++ Apisul/AppElevator.Application/ViewModel/ElevatorViewModelResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppElevator.Application.ViewModel
+{
+    public class ElevatorViewModelResult
+    {
+        //Obtem o andar menos utilizado pelos usuários;
+        public char GetAndarMenosUtilizado()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Obtém qual é o elevador mais frequentado e o período que se encontra maior fluxo;
+        public IEnumerable<string> GetElevadorMaisUtilizadoMaiorFluxo()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Obtém qual é o elevador menos frequentado e o período que se encontra menor fluxo;
+        public IEnumerable<string> GetElevadorMenosUsadoMenorFluxo()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Obtém qual o período de maior utilização do conjunto de elevadores;
+        public char GetPeriodoMaiorUsoConjunto()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Obtém qual o percentual de uso de cada elevador com relação a todos os serviços prestados;
+        public double GetPercentualUsoRelacaoServicos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Apisul/AppElevator/Controllers/ElevadorController.cs
+++ Apisul/AppElevator/Controllers/ElevadorController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppElevator.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -17,12 +21,47 @@ namespace AppElevator.Api.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        [HttpGet]
-        [Route("/moe")]
-        public string GetMoe()
+        //	Obtendo o andar menos utilizado pelos usuários;
+        [HttpGet("AndarMenosUtilizado")]
+        public char GetAndarMenosUtilizado()
         {
-            return "Ola Moe!";
+            ElevatorViewModelResult elevator = new ElevatorViewModelResult();
+            return elevator.GetAndarMenosUtilizado();
         }
+
+        //Obtendo o elevador mais frequentado e o período que se encontra maior fluxo;
+        [HttpGet("ElevadorMaisUsadoMaiorFluxo")]
+        public IEnumerable<string> GetElevadorMaisUsadoMaiorFluxo()
+        {
+            ElevatorViewModelResult elevator = new ElevatorViewModelResult();
+            return elevator.GetElevadorMaisUtilizadoMaiorFluxo();
+        }
+
+        //Obtendo o elevador menos frequentado e o período que se encontra menor fluxo;
+        [HttpGet("ElevadorMenosUsadoMenorFluxo")]
+        public IEnumerable<string> GetElevadorMenosUsadoMenorFluxo()
+        {
+            ElevatorViewModelResult elevator = new ElevatorViewModelResult();
+            return elevator.GetElevadorMenosUsadoMenorFluxo();
+        }
+
+        //Obtendo o o período de maior utilização do conjunto de elevadores;
+        [HttpGet("PeriodoMaiorUtilizacao")]
+        public char GetPeriodoMaiorUtilizacaoConjunto()
+        {
+            ElevatorViewModelResult elevador = new ElevatorViewModelResult();
+            return elevador.GetPeriodoMaiorUsoConjunto();
+
+        }
+
+        //Obtendo o o percentual de uso de cada elevador com relação a todos os serviços prestados;
+        [HttpGet("PercentualUsoRelacao")]
+        public double GetPercentualUso()
+        {
+            ElevatorViewModelResult elevator = new ElevatorViewModelResult();
+            return elevator.GetPercentualUsoRelacaoServicos();
+        }
+
 
 
         // GET api/<ElevadorController>/5
